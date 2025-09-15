929d69391cb9b9bf19e3aca6ccade5029f6416dc
bimaardhia <141271625+bimaardhia@users.noreply.github.com>
2025-09-15 14:13:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -87,13 +87,8 @@ load_css()
 with st.sidebar:
     if "AZ_OPENAI_API_KEY" not in os.environ:
         default_openai_api_key = os.getenv("OPENAI_API_KEY") if os.getenv("OPENAI_API_KEY") is not None else ""  # only for development environment, otherwise it should return None
-        # with st.popover("🔐 OpenAI"):
-            # openai_api_key = st.text_input(
-            #     "Introduce your OpenAI API Key (https://platform.openai.com/)", 
-            #     value=default_openai_api_key, 
-            #     type="password",
-            #     key="openai_api_key",
-            # )
+        with st.popover("🔐 OpenAI"):
+            openai_api_key = default_openai_api_key
     else:
         openai_api_key, anthropic_api_key = None, None
         st.session_state.openai_api_key = None
